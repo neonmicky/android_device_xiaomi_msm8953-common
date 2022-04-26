e365b3f26adc4fad38acc00dabb4e4e825410628
neonmicky <neonmicky30@gmail.com>
2022-04-26 06:02:31 +0000

    msm8953-common: Drop Snap
    Signed-off-by: neonmicky <neonmicky30@gmail.com>

--- msm8953.mk
+++ msm8953.mk
@@ -130,8 +130,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     camera.msm8953 \
     libmm-qcamera \
-    libstdc++.vendor \
-    Snap
+    libstdc++.vendor
 
 # Display
 PRODUCT_PACKAGES += \
